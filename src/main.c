2f2d04c7f4bbca8a0681e4a8092cfba2b1ff75fe
ks <ksainte19@gmail.com>
2024-11-15 01:52:48 +0100

    ok

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ks19 <ks19@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:14:55 by ks19              #+#    #+#             */
-/*   Updated: 2024/11/14 14:21:18 by ks19             ###   ########.fr       */
+/*   Updated: 2024/11/15 01:51:30 by ks19             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,9 +68,6 @@ int	ft_check_map_left_over(t_map *map)
 int	ft_row_number(t_map *map)
 {
     map->row = 0;
-	map->fd = open(map->path, O_RDONLY);
-	if (map->fd == -1)
-        return (ft_map_error(1, map));
 	map->line = (char *)malloc(sizeof(char *));
 	if (!map->line)
         return (ft_map_error(0, map));
@@ -115,15 +112,150 @@ char *ft_check_args(int argc, char *str)
 	return (path);
 }
 
+int ft_open_path(char *str, int i)
+{
+    int fd;
+    
+    if (i < 4 && i >= 0)
+        str = str + 2;
+    if (i >= 4 && i < 6)
+        str = str + 1;
+    if ((*str < 9 && *str > 13) && *str != 32)
+        return (0);
+    while ((*str >= 9 && *str <= 13) || *str == 32)
+        str++;
+	fd = open(str, O_RDONLY);
+	if (fd == -1)
+    {
+        ft_system_error();
+        return (0);
+    }
+    close (fd);
+    return (1);
+}
+
+int ft_element_is_valid(char *str)
+{
+    int value;
+    char **prefix;
+    int i;
+
+    prefix[0] = "NO";
+    prefix[1] = "SO";
+    prefix[2] = "WE";
+    prefix[3] = "EA";
+    prefix[4] = "F";
+    prefix[5] = "C";
+    while ((*str >= 9 && *str <= 13) || *str == 32)
+        str++;
+    value = -1;
+    i = 0;
+    while (value != 0 && i < 4)
+    {
+        value = ft_strncmp(prefix[i], str, 2);
+        i++;
+    }
+    while (value != 0 && i < 6)
+    {
+        value = ft_strncmp(prefix[i], str, 1);
+        i++;
+    }
+    if (value == 0 && ft_open_path(str, i - 1))
+        return (i - 1);
+    return (-1);
+}
+
+int ft_check_array(int array[6])
+{
+    int i;
+    int j;
+    int duplicates;
+
+    i = 0;
+    duplicates = 0;
+    while (i < 6)
+    {
+        j = 0;
+        while (j < 6)
+        {
+            if (i == array[j])
+                duplicates++;
+            if (duplicates == 2)
+                return (0);
+            j++;
+        }
+        i++;
+        duplicates = 0;
+    }
+    return (1);
+}
+
+int ft_free_line(t_map *map)
+{
+    free(map->line);
+    return (1);
+}
+
+int ft_textures_and_colors(t_map *map)
+{
+    int array[6];
+    int i;
+
+    i = 0;
+    while (i++ < 6)
+        array[i] = 0;
+    i = 0;
+	while (map->line != NULL)
+	{
+		free(map->line);
+		map->line = get_next_line(map->fd);
+        if (map->line && *map->line != '\n' && i == 6)
+            return (-1);
+        if (map->line && *map->line != '\n')
+        {
+            array[i] = ft_element_is_valid(map->line);
+            if (array[i] == -1)
+                return (-2);
+            i++;
+        }
+	}
+    if (!ft_check_array(array))
+        return (-3);
+    return (1)
+}
+
+char *ft_elements_to_parse(t_map *map)
+{
+    int value;
+
+    value = 1;
+	map->line = (char *)malloc(sizeof(char *));
+	if (!map->line)
+        return (ft_map_error(0, map));
+    value = ft_textures_and_colors(map);
+    if (value == -1 && ft_free_line(map))
+        return (ft_custom_error("There are more than 6 elements!\n"));
+    else if (value == -2 && ft_free_line(map))
+        return (ft_custom_error("Invalid element or Can't open path!\n"));
+    else if (value == -3)
+        return (ft_custom_error("There are duplicates in the elements!\n"));
+        
+}
+
 int main(int argc, char **argv)
 {
 	t_map   map;
 
     if(!(map.path = ft_check_args(argc, argv[1])))
         return (0);
-    if(!(ft_row_number(&map)))
+	map.fd = open(map.path, O_RDONLY);
+	if (map.fd == -1)
+        return (ft_map_error(1, &map));
+    if(!ft_elements_to_parse(&map))
         return (0);
-    ft_free(&map);
+    // if(!(ft_row_number(&map)))
+    //     return (0);
+    // ft_free(&map);
     return (1);
     
 }
