c388f41be0370b9c5d7f75565697310e4db6801f
ks <ksainte19@gmail.com>
2024-11-22 10:56:21 +0100

    ok

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ks19 <ks19@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:14:55 by ks19              #+#    #+#             */
-/*   Updated: 2024/11/21 17:33:12 by ks19             ###   ########.fr       */
+/*   Updated: 2024/11/22 10:52:03 by ks19             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,28 +90,28 @@ void	ft_find_start_pos(t_map *map)
 
 int ft_copy_to_data(t_map *map, t_data *data)
 {
-    int	x;
+    int i;
     
     data->s_x = map->s_x;
     data->s_y = map->s_y;
     data->row = map->row;
-    data->F[3] = map->F[3];
-    data->C[3] = map->C[3];
+	data->tab = ft_calloc(map->row + 1, sizeof(char *));
+	if (!data->tab)
+		return (0);
+    i = -1;
+	while (map->tab[++i])
+		data->tab[i] = ft_strdup(map->tab[i]);
+	data->tab[i] = NULL;
+    i = -1;
+    while (++i < 3)
+        data->F[i] = map->F[i];
+    i = -1;
+    while (++i < 3)
+        data->C[i] = map->C[i];
     data->NO = ft_strdup(map->NO);
     data->SO = ft_strdup(map->SO);
     data->WE = ft_strdup(map->WE);
     data->EA = ft_strdup(map->EA);
-	data->tab = ft_calloc(map->row + 1, sizeof(char *));
-	if (!data->tab)
-		return (0);
-    x = 0;
-	while (data->tab[x])
-	{
-		data->tab[x] = ft_strdup(map->tab[x]);
-		x++;
-	}
-	data->tab[x] = NULL;
-    ft_free(map);
     return (1);
 } 
 
@@ -125,10 +125,10 @@ int	ft_map_playable(t_map *map, t_data *data)
     if (!ft_has_valid_path(map, map->s_y, map->s_x) && ft_free_table(map->tmp))
         return (ft_map_error(6));
     ft_free_table(map->tmp);
-    // if (!ft_copy_to_data(map, data))
-    //     return(ft_map_error(7));
+    if (!ft_copy_to_data(map, data))
+        return(ft_map_error(0));
     write(1, "The player has a playable map!\n",32);
-    return (1);
+    return (ft_free(map));
 }
 
 int ft_free_data(t_data *data)
@@ -143,22 +143,19 @@ int ft_free_data(t_data *data)
 
 void ft_print_data(t_data *data)
 {
-    int i = 0;
-    while(i < 3)
-    {
+    int i = -1;
+    while(++i < 3)
         printf("F %d\n", data->F[i]);
-        i++;
-    }
-    i = 0;
-    while(i < 3)
-    {
+    i = -1;
+    while(++i < 3)
         printf("C %d\n", data->C[i]);
-        i++;
-    }
     printf("NO %s\n", data->NO);
     printf("SO %s\n", data->SO);
     printf("EA %s\n", data->EA);
     printf("WE %s\n", data->WE);
+    printf("row is %d\n", data->row);
+    printf("s->y is %d\n", data->s_y);
+    printf("s->x is %d\n", data->s_x);
     printf("-----------------\n");
     ft_print_table(data->tab);
 }
@@ -172,8 +169,7 @@ int main(int argc, char **argv)
         return (0);
     if((!ft_parse_valid(&map) || !ft_map_playable(&map, &data)) && ft_free(&map))
         return (0);
-    ft_free(&map);
-    // ft_print_data(&data);
-    // ft_free_data(&data);
+    ft_print_data(&data);
+    ft_free_data(&data);
     return (1);
 }
