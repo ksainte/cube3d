c63e24ac38b5cfcfb311e556e3e5f7a512fae89b
Aurelio Sideris <asideris@mi-r7-p1.s19.be>
2024-11-22 12:59:47 +0100

    s

--- src/main.c
+++ src/main.c
@@ -3,24 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ks19 <ks19@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: asideris <asideris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:14:55 by ks19              #+#    #+#             */
-/*   Updated: 2024/11/22 11:06:22 by ks19             ###   ########.fr       */
+/*   Updated: 2024/11/22 12:58:19 by asideris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cube_3d.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_map   map;
-    t_data data;
+	t_map map;
+	t_data data;
 
-    if(!(map.path = ft_check_args(argc, argv[1])))
-        return (0);
-    if((!ft_parse_valid(&map) || !ft_map_playable(&map, &data)) && ft_free(&map))
-        return (0);
-    ft_free_data(&data);
-    return (1);
+	if (!(map.path = ft_check_args(argc, argv[1])))
+		return (0);
+	if ((!ft_parse_valid(&map) || !ft_map_playable(&map, &data))
+		&& ft_free(&map))
+		return (0);
+	ft_print_data(&data);
+	ft_free_data(&data);
+	return (1);
 }
